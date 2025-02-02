7b295b8eae4a48b12892e0a6142407e01e839bc1
Li[李] <aliimrandtb@gmail.com>
2025-02-02 14:07:41 +0700

    [ADD] alnas_roman_sequence: add module for extend sequence number for support roman numbering

---
+++ alnas_roman_sequence/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Roman Number Sequence",
+
+    'summary': "Use Roman Number for Sequence",
+
+    'description': """
+        If you want to use Roman number as a sequence number, please set 'sequence size' equal to '1'.
+        If you want to use roman number in prefix/suffix, you can add prefix in the legend. example :
+        Day: %(day)s => Day in Roman: %(roman_day)s, etc.
+        
+    """,
+
+    'author': "Ali Ns",
+    'website': "https://www.github.com/alienyst",
+    'maintainer': ['Ali Ns'],
+    'category': 'Extra Tools',
+    'version': '17.0.0.0',
+
+    'depends': ['base'],
+    'license': 'AGPL-3',
+
+    'data': [
+        'views/ir_sequence_views.xml',
+    ],
+    
+
+}
+
---
+++ alnas_roman_sequence/models/ir_sequence.py
@@ -0,0 +1,102 @@
+# Copyright 2025 Ali Ns
+# @author: Ali Ns <aliimrandtb@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from datetime import datetime
+import pytz
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+ROMAN_NUMERAL_MAP = (
+            ('M', 1000),
+            ('CM', 900),
+            ('D', 500),
+            ('CD', 400),
+            ('C', 100),
+            ('XC', 90),
+            ('L', 50),
+            ('XL', 40),
+            ('X', 10),
+            ('IX', 9),
+            ('V', 5),
+            ('IV', 4),
+            ('I', 1)
+        )
+
+
+class IrSequence(models.Model):
+    _inherit = 'ir.sequence'
+    
+    is_roman = fields.Boolean()
+    
+    
+    def to_roman(self, number):
+        """convert integer to roman numeral"""
+        number = int(number)
+        if not (0 <= number < 5000):
+            raise UserError("number out of range (must be 0..4999)")
+
+        if number == 0:
+            return 'N'
+        
+        result = []
+        for numeral, integer in ROMAN_NUMERAL_MAP:
+            while number >= integer:
+                result.append(numeral)
+                number -= integer
+        
+        return ''.join(result)
+    
+    @api.constrains('is_roman')
+    def _check_padding(self):
+        for rec in self:
+            if rec.is_roman and rec.padding != 1:
+                raise UserError('To use roman numbering, sequence size must be equal to 1')
+            
+    def get_next_char(self, number_next):
+        #number_next is (15,)
+        #ensure number_next is integer
+        if self.is_roman:
+            number_next_roman = self.to_roman(number_next[0]) 
+            interpolated_prefix, interpolated_suffix = self._get_prefix_suffix()
+            return interpolated_prefix + number_next_roman + interpolated_suffix
+
+        return super().get_next_char(number_next)
+        
+    # add conversion to roman
+    def _get_prefix_suffix(self, date=None, date_range=None):
+        def _interpolate(s, d):
+            return (s % d) if s else ''
+
+        def _interpolation_dict():
+            now = range_date = effective_date = datetime.now(pytz.timezone(self._context.get('tz') or 'UTC'))
+            if date or self._context.get('ir_sequence_date'):
+                effective_date = fields.Datetime.from_string(date or self._context.get('ir_sequence_date'))
+            if date_range or self._context.get('ir_sequence_date_range'):
+                range_date = fields.Datetime.from_string(date_range or self._context.get('ir_sequence_date_range'))
+
+            sequences = {
+                'year': '%Y', 'month': '%m', 'day': '%d', 'y': '%y', 'doy': '%j', 'woy': '%W',
+                'weekday': '%w', 'h24': '%H', 'h12': '%I', 'min': '%M', 'sec': '%S'
+            }
+            res = {}
+            for key, format in sequences.items():
+                res[key] = effective_date.strftime(format)
+                res['range_' + key] = range_date.strftime(format)
+                res['current_' + key] = now.strftime(format)
+                res['roman_' + key] = self.to_roman(res[key])
+                res['roman_range_' + key] = self.to_roman(res['range_' + key])
+                res['roman_current_' + key] = self.to_roman(res['current_' + key])
+                
+            return res
+
+        self.ensure_one()
+        d = _interpolation_dict()
+        try:
+            interpolated_prefix = _interpolate(self.prefix, d)
+            interpolated_suffix = _interpolate(self.suffix, d)
+        except (ValueError, TypeError):
+            raise UserError(_('Invalid prefix or suffix for sequence “%s”', self.name))
+        return interpolated_prefix, interpolated_suffix
